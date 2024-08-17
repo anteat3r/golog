788d41e6c0f3042262ff1972e47ca288a16938a0
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-08-17 15:56:39 +0200

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anteat3r/golog
+
+go 1.22.6
---
+++ log.go
@@ -0,0 +1,48 @@
+package golog
+
+import (
+  "runtime"
+  "strings"
+  "fmt"
+  "time"
+)
+
+func logMsg(msg string, v ...any) {
+  fileres := ""
+  pc := make([]uintptr, 10)
+  n := runtime.Callers(3, pc)
+  var frames *runtime.Frames
+  if n == 0 {
+    goto fileend
+  }
+  pc = pc[:n]
+  frames = runtime.CallersFrames(pc)
+  for {
+    frame, more := frames.Next()
+    if strings.Contains(frame.File, "asm_amd64") { break }
+    if strings.Contains(frame.File, "go/pkg/mod") { break }
+    fileres += fmt.Sprintf("%v:%v -> ", frame.File, frame.Line)
+    if !more { break }
+  }
+
+  fileend:
+  res := ""
+  for _, e := range v {
+    res += fmt.Sprintf("%v %T, ", e, e)
+  }
+  fmt.Printf(
+    "%v %v%v: %v\n\n",
+    strings.Split(time.Now().String(), ".")[0],
+    fileres,
+    msg,
+    res,
+  )
+}
+
+func LogError(v ...any) {
+  logMsg("ERROR", v...)
+}
+
+func LogInfo(v ...any) {
+  logMsg("INFO", v...)
+}
